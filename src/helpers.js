a69ee63788cb44fe88236b146c388d6befeace27
Luke <lukelee01@gmail.com>
2019-06-06 23:21:49 -0400

    feature: create getText(), getCount() in loadUrl() in helper.js.

--- src/helpers.js
+++ src/helpers.js
@@ -16,10 +16,29 @@ export const typeText = async (page, selector, text) => {
     await page.waitForSelector(selector);
     await page.type(selector, text);
   } catch (e) {
-    throw new Error(`Could not type ${text} into ${selector}`);
+    throw new Error(`Could not type ${text} into ${selector}.`);
   }
 };
 
 export const loadUrl = async (page, url) => {
   await page.goto(url, {waitUntil: 'networkidle0'});
+};
+
+export const getText = async (page, selector) => {
+  try {
+    await page.waitForSelector(selector);
+    return page.$eval(selector, e => e.innerHTML);
+  } catch (e) {
+    throw new Error(`Couldn't get text from ${selector}.`);
+  }
+};
+
+// TODO: try to understand line by line.
+export const getCount = async (page, selector) => {
+  try {
+    await page.waitForSelector(selector);
+    return page.$$eval(selector, e => e.length);
+  } catch (e) {
+    throw new Error(`Cannot get count of selector: ${selector}`);
+  }
 };
