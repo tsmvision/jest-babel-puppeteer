b52c161ab594f0399e274ee506285486908ae39f
Luke <lukelee01@gmail.com>
2019-06-06 22:37:40 -0400

    feature: add some sample puppeteer unit test with dev.to.

--- src/index.test.js
+++ src/index.test.js
@@ -1,50 +1,61 @@
 import puppeteer from 'puppeteer';
-import {
-    isTSAnyKeyword,
-    exportAllDeclaration
-} from '@babel/types';
 
 describe('My first puppeteer test', () => {
-    let browser;
-    let page;
+  let browser;
+  let page;
 
-    beforeAll(async () => {
-        browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 0,
-            devtools: false,
-            timeout: 10000,
-        });
-        page = await browser.newPage();
-        await page.setViewport({
-            width: 1024,
-            height: 768
-        });
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 0,
+      devtools: false,
+      timeout: 10000,
     });
-
-    afterAll(async () => {
-        await browser.close();
+    page = await browser.newPage();
+    await page.setViewport({
+      width: 1024,
+      height: 768
     });
+  });
 
-    it('my first test step', async () => {
-        await page.goto('http://dev.to/');
-        await page.waitForSelector('#nav-search');
+  afterAll(async () => {
+    await browser.close();
+  });
 
-        const url = await page.url();
-        const title = await page.title();
+  it('my first test step', async () => {
+    await page.goto('http://dev.to/');
+    await page.waitForSelector('#nav-search');
 
-        expect(url).toContain('dev');
-        expect(title).toContain('DEV Community');
-    });
+    const url = await page.url();
+    const title = await page.title();
 
-    it('browser reload', async () => {
-        await page.reload();
-        await page.waitForSelector('#page-content');
+    expect(url).toContain('dev');
+    expect(title).toContain('DEV Community');
+  });
 
-        const url = await page.url();
-        const title = await page.title();
+  it('browser reload', async () => {
+    await page.reload();
+    await page.waitForSelector('#page-content');
 
-        expect(url).toContain('dev');
-        expect(title).toContain('Community');
-    });
+    const url = await page.url();
+    const title = await page.title();
+
+    expect(url).toContain('dev');
+    expect(title).toContain('Community');
+  });
+
+  it('click method', async () => {
+    await page.goto('http://dev.to/');
+    await page.waitForSelector('#write-link');
+    await page.click('#write-link');
+    await page.waitForSelector('.registration-rainbow');
+  });
+
+  it('submit searchbox', async () => {
+    await page.goto('https://dev.to/');
+    await page.waitForSelector('#nav-search');
+    await page.type('#nav-search', "Javascript");
+    await page.keyboard.press('Enter');
+    await page.waitForSelector('#articles-list');
+  });
 });
